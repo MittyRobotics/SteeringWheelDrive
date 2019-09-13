0c0bb8a7cca5aec98229ffb76060b2f521ed74b8
Krish Prasad <kp.kriish@gmail.com>
2019-09-12 17:08:19 -0700

    Update JoystickDrive_CompassSteering.java
    started joystick drive; compass drive

--- src/main/java/frc/robot/JoystickDrive_CompassSteering.java
+++ src/main/java/frc/robot/JoystickDrive_CompassSteering.java
@@ -1,4 +1,51 @@
 package frc.robot;
 
-public class JoystickDrive_CompassSteering {
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.DriveTrain;
+import frc.robot.OI;
+
+public class JoystickDrive_CompassSteering extends Command { //example command
+
+    private double turn;
+    private double e = 0.7;
+
+    public JoystickDrive_CompassSteering(){
+        requires(DriveTrain.getInstance());
+    }
+
+    @Override
+    protected void initialize(){
+
+    }
+
+    @Override
+    protected void execute(){
+
+        turn = OI.getInstance().getSteeringWheel().getX();
+        DriveTrain.getInstance().tankDrive(turn, -turn);
+
+        double speed = OI.getInstance().getJoystick().getY();
+
+        double newSpeed = speed*e;
+        double newTurn = turn * (1-e);
+
+
+        DriveTrain.getInstance().tankDrive(newSpeed + newTurn, newSpeed - newTurn);
+    }
+
+    @Override
+    protected void end(){
+
+    }
+
+    @Override
+    protected void interrupted(){
+
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
 }
